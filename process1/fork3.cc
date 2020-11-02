5e917fa9db2a55dfac9d1ed5c9677192c4a9b96c
Eddie Kohler <ekohler@gmail.com>
2020-11-02 16:56:55 -0500

    Process1 pre-work.

---
+++ process1/fork3.cc
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <cstdio>
+#include <cassert>
+
+int main() {
+    printf("Hello from initial pid %d\n", getpid());
+
+    pid_t p1 = fork();
+    assert(p1 >= 0);
+
+    pid_t p2 = fork();
+    assert(p2 >= 0);
+
+    printf("Hello from final pid %d\n", getpid());
+}
